4a611561c4ccce962d9a31b15447f329394ac9f0
kawa0622 <fmhcf972@yahoo.co.jp>
2025-04-06 13:05:42 +0900

    phpカリキュラム

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+  <p>この文章はHTMLで出力しています。</p>
+  <p>
+    <?php
+        // PHPのコードは<?phpから始まります。
+        // echoは文字列を出力するための関数です。
+        echo "この文章はPHPで出力しています。";
+    ?>
+  </p>
+  </body>
+</html>
