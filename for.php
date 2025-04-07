55eca4ba7eefcd6479c2b84b5abd942ec0d7790b
kawa0622 <fmhcf972@yahoo.co.jp>
2025-04-07 19:48:30 +0900

    for文を書いてみよう

---
+++ for.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+   <meta charset="UTF-8">
+   <title>PHP基礎編</title>
+</head>
+
+<body>
+   <p>
+    <?php
+    // 1～10までの整数を順番に出力する
+    for ($i = 1; $i <=10; $i++) {
+      echo $i . '<br>';
+    }
+    ?>
+    </p>
+    <p>
+    <?php
+    for ($i = 1; $i <= 10; $i++) {
+      // 変数$numに1～20までのランダムな整数を代入する
+      $num = mt_rand(1,20);
+
+     echo "{$i}回目の結果は{$num}です。<br>";
+
+      // 変数$numが20であれば、break文で繰り返し処理を強制終了する
+      if ($num === 20) {
+        echo "20が出たので繰り返し処理を強制終了します。";
+        break;
+      }
+    }
+    ?>
+    </p>
+    </body>
+</html>
