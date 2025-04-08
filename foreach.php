5c8d7b581eb7fbcbd6f6184d71fd8425f3a1cde3
kawa0622 <fmhcf972@yahoo.co.jp>
2025-04-08 18:18:42 +0900

    12.5 セレクトボックスの選択肢をforeach文で作ってみよう,break文

--- foreach.php
+++ foreach.php
@@ -38,5 +38,46 @@
       }
         ?>
     </p>
+    
+    <p>
+      <form>
+        <select>
+          <?php
+          $birthplace = [
+            '北海道地方',
+            '東北地方',
+            '関東地方',
+            '中部地方',
+            '近畿地方',
+            '中国地方・四国地方',
+            '九州地方',
+          ];
+          // 配列$user_nameの要素を一つずつ順番に出力する
+          foreach ($birthplace as $birthplace) {
+            echo "<option>{$birthplace}</option>";
+          }
+          ?>
+        </select>
+      </form>
+
+      <p>
+        <?php
+        $user_name = ['侍太郎', '侍一郎', '侍二郎', '侍三郎', '侍四郎'];
+
+        //検索したいユーザー名を代入する変数
+        $target = '侍二郎';
+
+        foreach ($user_name as $user_name) {
+          echo $user_name . '<br>';
+
+          //変数$user_nameと変数$targetが一致すれば、break文で繰り返し処理を強制終了する
+          if ($user_name === $target) {
+            echo "{$target}さんが見つかったので、繰り返し処理を強制終了します";
+            break;
+          }
+        }
+        ?>
+        </p>
+
     </body>
 </html>
