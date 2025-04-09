de6d31fce39d7cd3b379d64de60c682eea38026b
kawa0622 <fmhcf972@yahoo.co.jp>
2025-04-09 20:40:12 +0900

    複数の引数を受け取る関数

--- function.php
+++ function.php
@@ -40,6 +40,16 @@
 
     //関数を呼び出し、引数として購入金額を渡す  
     calculate_total(1200);
+
+    //与えられた引数$priceと引数$shipping_feeを加算し、その値を出力する
+    function add_two_arguments($price,$shipping_fee){
+        $total = $price + $shipping_fee;
+
+        echo $total . '円<br>';
+    }
+
+    //関数を呼び出し、引数として購入金額と送料を渡す
+    add_two_arguments(1200,500);
     ?>
    </p>
 </body>
