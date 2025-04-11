a7a75edebc96dc425e220920d90608a0505844ec
kawa0622 <fmhcf972@yahoo.co.jp>
2025-04-11 21:07:37 +0900

    メソッドを使ってみよう

--- class.php
+++ class.php
@@ -13,10 +13,25 @@
     class product {
       // プロパティを定義する
       public $name; // 商品名
+
+      // メソッドを定義する
+      public function set_name(string $name) {// stringは文字列のこと
+        $this->name = $name; // プロパティに値を代入する
+      }
+      public function show_name() {
+        echo $this->name . '<br>'; // プロパティの値を表示する
+      }
     }
 
     // インスタンスを生成する
-    $shampoo = new product();
+    $coffee = new Product();
+
+    //メソッドにアクセスして実行する  
+    $coffee->set_name('コーヒー'); // メソッドにアクセスし、値を代入する
+    $coffee->show_name(); // メソッドにアクセスし、値を表示する
+
+    // インスタンスを生成する
+    $shampoo = new Product();
 
     // プロパティにアクセスし、値を代入する
     $shampoo->name = 'シャンプー';
