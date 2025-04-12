8460fce7be0bd90153448f69b27e7c104588ce69
kawa0622 <fmhcf972@yahoo.co.jp>
2025-04-12 19:42:08 +0900

    演習

--- class.php
+++ class.php
@@ -64,5 +64,102 @@
       print_r($user);
       ?>
     </p>
+    <p>
+      <?php
+     
+     // 演習(AI、各プロパティに直接アクセスして、値を代入する)
+      class Eve_nt {
+        public $name;
+        public $date;
+        public $location;
+    
+        // コンストラクタで各プロパティに値を代入
+        public function __construct($name, $date, $location) {
+            $this->name = $name;
+            $this->date = new DateTime($date);
+            $this->location = $location;
+        }
+    
+        // 開催日までの日数を返すメソッド
+        public function daysUntilEvent() {
+            $today = new DateTime();
+            $interval = $today->diff($this->date);
+            return $interval->days;
+        }
+    }
+    
+    // インスタンスの作成
+    $event = new Eve_nt("プログラミング初心者向けセミナー", "2025-05-10", "東京国際フォーラム");
+    
+    // プロパティを出力
+    echo "イベント名: " . $event->name . "<br>";
+    echo "開催日: " . $event->date->format('Y-m-d') . "<br>";
+    echo "開催場所: " . $event->location . "<br>";
+    
+    // 開催日までの日数を出力
+    echo "開催日まであと " . $event->daysUntilEvent() . " 日です。<br>";
+    ?>
+</p>
+
+
+ <!-- 演習(各プロパティに間接的にアクセスして、値を代入する) -->
+    <p>
+      <?php
+    class Event
+{
+    private $name;
+    private $date;
+    private $location;
+
+    // コンストラクタ
+    public function __construct($name, $date, $location)
+    {
+        $this->name = $name;
+        $this->date = new DateTime($date);
+        $this->location = $location;
+    }
+
+    // 各プロパティの値を取得するメソッド
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getDate()
+    {
+        return $this->date->format('Y-m-d');
+    }
+
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    // 開催日までの日数を返すメソッド
+    public function daysUntilEvent()
+    {
+        $today = new DateTime();
+        $interval = $today->diff($this->date);
+        return $interval->days;
+    }
+}
+
+// 実行結果
+$event = new Event(
+    'プログラミング初心者向けセミナー', 
+    '2024-12-15', // 現在より未来の日付を設定
+    '東京国際フォーラム'
+);
+
+// 各プロパティにアクセスして値を出力
+echo "イベント名: " . $event->getName() . PHP_EOL;
+echo "開催日: " . $event->getDate() . PHP_EOL;
+echo "開催場所: " . $event->getLocation() . PHP_EOL;
+
+// 開催日までの日数を出力
+echo "開催日までの日数: " . $event->daysUntilEvent() . "日" . PHP_EOL;
+
+?>
+</p>
     </body>
 </html>
